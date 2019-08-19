20eaf8a672c9802c16f05255cf450806d3d72d20
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-19 18:52:46 -0400

    some of the gate tests working, dashboard tests working, some of display component tests working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Controls />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // it('gate cannot be closed if locked', () => {
+  //   const { queryByText, getByTestId } = render(<Controls />);
+  //
+  //   fireEvent.click(getByTestId(/controlsButton2/i));
+  //   fireEvent.click(getByTestId(/controlsButton1/i));
+  //   fireEvent.click(getByTestId(/controlsButton2/i));
+  //
+  //   expect(queryByText(/Open Gate/i)).toBeTruthy();
+  // });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+describe('<App />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Display />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('displays "open" by default', () => {
+    const { queryByText } = render(<Display />);
+    expect(queryByText(/Open/i)).toBeTruthy();
+  });
+
+  it('displays "unlocked" by default', () => {
+    const { queryByText } = render(<Display />);
+    expect(queryByText(/Unlocked/i)).toBeTruthy();
+  });
+});
